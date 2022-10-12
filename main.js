639b6b11926e87c1df1d4261dcf86e20fbc5c638
Aleksandra Kirillova <kiri2lova@gmail.com>
2022-10-12 16:34:04 +0300

    feat: добавить правки

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let popup = document.querySelector('.popup'); // Само окно
 let openPopupButton = document.querySelector('.profile__edit-button'); // Кнопки для показа окна
 let closePopupButton = document.querySelector('.popup__close-button'); // Кнопка для скрытия окна
 let savePopupButton = document.querySelector('.popup__save-button');
+let editForm = document.querySelector('.popup');
 
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
@@ -16,17 +17,20 @@ function openPopup() {
   inputProfileName.value = profileName.textContent;
   inputProfileDescription.value = profileDescription.textContent;
 }
+
 function closePopup(e) {
   e.preventDefault();
   popupBg.style = 'visibility: hidden';
 }
 
 function closeAndSave(e) {
-  closePopup(e);
   profileName.textContent = inputProfileName.value;
   profileDescription.textContent = inputProfileDescription.value;
+  closePopup(e);
 }
+
 
 openPopupButton.addEventListener('click', openPopup);
 closePopupButton.addEventListener('click', closePopup);
-savePopupButton.addEventListener('click', closeAndSave);
+editForm.addEventListener('submit', closeAndSave);
+
