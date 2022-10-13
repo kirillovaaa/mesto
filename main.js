cba5f95f712b992c8592989a2e49e0aaa36d3618
Aleksandra Kirillova <kiri2lova@gmail.com>
2022-10-13 07:54:13 +0300

    fix: переделать структуру popup по БЭМ

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
-let popupBg = document.querySelector('.popup__bg'); // Фон попап окна
-let popup = document.querySelector('.popup'); // Само окно
+let popupBg = document.querySelector('.popup'); // Весь попап с фоном
+let editForm = document.querySelector('.popup__form'); // Форма редактирования
 let openPopupButton = document.querySelector('.profile__edit-button'); // Кнопки для показа окна
 let closePopupButton = document.querySelector('.popup__close-button'); // Кнопка для скрытия окна
-let savePopupButton = document.querySelector('.popup__save-button');
-let editForm = document.querySelector('.popup');
 
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
 let inputProfileName = document.querySelector('.popup__input-name');
 let inputProfileDescription = document.querySelector('.popup__input-description');
-
-let popupOpen = false;
 
 function openPopup() {
   popupBg.style = 'visibility: initial';
