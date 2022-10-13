ea0fcdac935c797f5fad49828118934063a4e3e7
Aleksandra Kirillova <kiri2lova@gmail.com>
2022-10-13 19:59:55 +0300

    fix: внести правки-2

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,19 @@
 let popupBg = document.querySelector('.popup'); // Весь попап с фоном
-let editForm = document.forms.profile; // Форма редактирования
 let openPopupButton = document.querySelector('.profile__edit-button'); // Кнопки для показа окна
 let closePopupButton = document.querySelector('.popup__close-button'); // Кнопка для скрытия окна
 
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
 
+let editForm = document.forms.profile;
+let inputName = editForm.elements.name;
+let inputDescription = editForm.elements.description;
+
+
 function openPopup() {
   popupBg.classList.add('popup_opened');
-  editForm.elements.name.value = profileName.textContent;
-  editForm.elements.description.value = profileDescription.textContent;
+  inputName.value = profileName.textContent;
+  inputDescription.value = profileDescription.textContent;
 }
 
 function closePopup() {
@@ -17,9 +21,9 @@ function closePopup() {
 }
 
 function closeAndSave(e) {
+  profileName.textContent = inputName.value;
   e.preventDefault();
-  profileName.textContent = editForm.elements.name.value;
-  profileDescription.textContent = editForm.elements.description.value;
+  profileDescription.textContent = inputDescription.value;
   closePopup();
 }
 
