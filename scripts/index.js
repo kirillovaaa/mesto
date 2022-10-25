6da0dc7c35e36cb1a4b31fe2ce1824d3bb39c884
Aleksandra Kirillova <kiri2lova@gmail.com>
2022-10-25 18:37:04 +0300

    feat: сделать программное заполнение сетки с местами

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,8 @@ let popupBg = document.querySelector('.popup'); // Весь попап с фон
 let openPopupButton = document.querySelector('.profile__edit-button'); // Кнопки для показа окна
 let closePopupButton = document.querySelector('.popup__close-button'); // Кнопка для скрытия окна
 
+let places = document.querySelector('.places');
+
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
 
@@ -32,3 +34,59 @@ openPopupButton.addEventListener('click', openPopup);
 closePopupButton.addEventListener('click', closePopup);
 editForm.addEventListener('submit', closeAndSave);
 
+const initialPlaces = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+function addPlaceToGrid(place) { 
+  const placeItem = document.createElement('div'); //создать элемент
+  placeItem.classList.add('places__item'); //добавить в класслист (add)
+
+  const image = document.createElement('img');
+  image.classList.add('places__image');
+  image.alt = place.name;
+  image.src = place.link;
+
+  const nameWrapper = document.createElement('div');
+  nameWrapper.classList.add('places__name-wrapper');
+
+  const name = document.createElement('h2'); 
+  name.classList.add('places__name'); 
+  name.textContent = place.name; // название места из массива 
+ 
+  const favButton = document.createElement('button');
+  favButton.classList.add('places__fav-button');
+  favButton.type = 'button';
+  favButton.addEventListener('click', function (event) { 
+    favButton.classList.toggle('places__fav-button_selected');
+  });
+
+  nameWrapper.append(name, favButton); // добавляем в nameWrapper name и favButton
+  placeItem.append(image, nameWrapper); // добавляем в placesItem image и nameWrapper
+  places.append(placeItem); //добавляем в places placeItem 
+}
+ 
+initialPlaces.forEach(addPlaceToGrid);
