323f3ecdc0759d45ad06e4c2be4c44c127728560
Aleksandra Kirillova <kiri2lova@gmail.com>
2022-10-27 13:38:36 +0300

    test

--- scripts/index.js
+++ scripts/index.js
@@ -71,30 +71,30 @@ editProfileForm.addEventListener("submit", closeAndSaveProfile);
 addPlaceForm.addEventListener("submit", closeAndSavePlace);
 
 const initialPlaces = [
-  // {
-  //   name: "Архыз",
-  //   link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-  // },
-  // {
-  //   name: "Челябинская область",
-  //   link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-  // },
-  // {
-  //   name: "Иваново",
-  //   link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-  // },
-  // {
-  //   name: "Камчатка",
-  //   link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-  // },
-  // {
-  //   name: "Холмогорский район",
-  //   link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-  // },
-  // {
-  //   name: "Байкал",
-  //   link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-  // },
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
 ];
 
 function addPlaceToGrid(place) {
