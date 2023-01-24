d84ae3aa6c7488f280e82e33f7faa870dfccb496
Aleksandra Kirillova <kiri2lova@gmail.com>
2023-01-24 20:57:17 +0400

    понятные названия переменных, комментарии в коде

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,148 @@
 import Card from "./Card.js";
 import FormValidator from "./FormValidator.js";
 
+//
+//
+// Валидаторы форм
+//
+
+/** Валидатор формы профиля */
+const profileFormValidator = new FormValidator({
+  formSelector: "#form-profile", // id формы
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__save-button",
+  inactiveButtonClass: "popup__save-button_inactive",
+  errorClass: "popup__input-error_active",
+});
+/** Валидатор формы места */
+const placeFormValidator = new FormValidator({
+  formSelector: "#form-place", // id формы
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__save-button",
+  inactiveButtonClass: "popup__save-button_inactive",
+  errorClass: "popup__input-error_active",
+});
+// Активация валидаторов форм
+profileFormValidator.enableValidation();
+placeFormValidator.enableValidation();
+
+//
+//
+// Попапы
+//
+
+/** Попап с формой редактирования профиля */
+const profilePopup = document.querySelector("#popup-profile");
+/** Попап с формой добавления места */
+const placePopup = document.querySelector("#popup-place");
+/** Попап с картинкой */
+export const imagePopup = document.querySelector("#popup-image");
+
+/** Обработчик нажатия на кнопку Esc */
+function handlePressEsc(e) {
+  if (e.key === "Escape") {
+    const openedPopup = document.querySelector(".popup_opened");
+    closePopup(openedPopup);
+  }
+}
+/** Функция открытия попапа */
+export function openPopup(popupElement) {
+  popupElement.classList.add("popup_opened");
+  document.addEventListener("keydown", handlePressEsc);
+}
+/** Функция закрытия попапа */
+function closePopup(popupElement) {
+  popupElement.classList.remove("popup_opened");
+  document.removeEventListener("keydown", handlePressEsc);
+}
+
+//
+//
+// Профиль
+//
+
+/** Кнопка открытия формы профиля */
+const openProfileFormButton = document.querySelector(".profile__edit-button");
+
+/** Имя профиля */
+const profileNameLabel = document.querySelector(".profile__name");
+/** Описание профиля */
+const profileDescriptionLabel = document.querySelector(".profile__description");
+
+/** Форма профиля */
+const profileForm = document.forms.profile;
+/** Поле ввода имени профиля */
+const profileNameField = profileForm.elements.name;
+/** Поле ввода описания профиля */
+const profileDescriptionField = profileForm.elements.description;
+
+/** Обработчик нажатия на кнопку редактирования профиля */
+function openProfilePopup() {
+  openPopup(profilePopup);
+  profileNameField.value = profileNameLabel.textContent;
+  profileDescriptionField.value = profileDescriptionLabel.textContent;
+}
+
+/** Обработчик отправки формы редактирования профиля */
+function closeAndSaveProfile(e) {
+  e.preventDefault();
+  profileNameLabel.textContent = profileNameField.value;
+  profileDescriptionLabel.textContent = profileDescriptionField.value;
+  closePopup(profilePopup);
+}
+
+/** Слушатель нажатия на кнопку редактирования профиля */
+openProfileFormButton.addEventListener("click", openProfilePopup);
+/** Слушатель отправки формы редактирования профиля */
+profileForm.addEventListener("submit", closeAndSaveProfile);
+
+//
+//
+// Новое место
+//
+
+/** Кнопка открытия формы новой карточки */
+const openPlacePopupButton = document.querySelector(".profile__add-button");
+
+/** Форма нового места */
+const placeForm = document.forms.place;
+/** Поле ввода названия места */
+const placeNameField = placeForm.elements.name;
+/** Поле ввода ссылки на изображение */
+const placeLinkField = placeForm.elements.link;
+
+/** Обработчик открытия формы нового места */
+function handleOpenPlacePopupButton() {
+  placeNameField.value = "";
+  placeLinkField.value = "";
+  const popupPlace = document.querySelector("#popup-place");
+  const saveButton = popupPlace.querySelector(".popup__save-button");
+  saveButton.classList.add("popup__save-button_inactive");
+  saveButton.disabled = true;
+  openPopup(placePopup);
+}
+
+/** Обработчик отправки формы нового места */
+function handlePlaceSubmit(e) {
+  e.preventDefault();
+  let name = placeForm.elements.name.value;
+  let link = placeForm.elements.link.value;
+  let place = { name, link };
+  renderCard(place);
+  closePopup(placePopup);
+}
+
+/** Слушатель */
+openPlacePopupButton.addEventListener("click", handleOpenPlacePopupButton);
+/** Слушатель отправки формы места */
+placeForm.addEventListener("submit", handlePlaceSubmit);
+
+//
+//
+// Сетка карточек
+//
+
+/** Места, которые будут загружаться изначально */
 const initialPlaces = [
   {
     name: "Архыз",
@@ -28,111 +170,15 @@ const initialPlaces = [
   },
 ];
 
-const profileFormValidator = new FormValidator({
-  formSelector: "#form-profile", // id формы
-  inputSelector: ".popup__input",
-  submitButtonSelector: ".popup__save-button",
-  inactiveButtonClass: "popup__save-button_inactive",
-  errorClass: "popup__input-error_active",
-});
-
-const placeFormValidator = new FormValidator({
-  formSelector: "#form-place", // id формы
-  inputSelector: ".popup__input",
-  submitButtonSelector: ".popup__save-button",
-  inactiveButtonClass: "popup__save-button_inactive",
-  errorClass: "popup__input-error_active",
-});
-
-profileFormValidator.enableValidation();
-placeFormValidator.enableValidation();
-
-/** Попап с формой редактирования профиля */
-const profilePopup = document.querySelector("#popup-profile");
-/** Попап с формой добавления места */
-const placePopup = document.querySelector("#popup-place");
-/** Попап с картинкой */
-export const imagePopup = document.querySelector("#popup-image");
-
-/** Обработчик нажатия на кнопку Esc */
-function handlePressEsc(e) {
-  if (e.key === "Escape") {
-    const openedPopup = document.querySelector(".popup_opened");
-    closePopup(openedPopup);
-  }
-}
-
-/** Функция открытия попапа */
-export function openPopup(popupElement) {
-  popupElement.classList.add("popup_opened");
-  document.addEventListener("keydown", handlePressEsc);
-}
-
-/** Функция закрытия попапа */
-function closePopup(popupElement) {
-  popupElement.classList.remove("popup_opened");
-  document.removeEventListener("keydown", handlePressEsc);
-}
-
-const popupOpenProfileButton = document.querySelector(".profile__edit-button"); // Кнопки для показа окна
-const popupOpenPlaceButton = document.querySelector(".profile__add-button"); // Кнопки для показа окна
-
+/** Элемент сетки с карточками мест */
 const places = document.querySelector(".places");
 
-const profileName = document.querySelector(".profile__name");
-const profileDescription = document.querySelector(".profile__description");
-
-//форма профиля
-const profileFormEdit = document.forms.profile;
-//поля формы профиля
-const inputName = profileFormEdit.elements.name;
-const inputDescription = profileFormEdit.elements.description;
-
-// форма добавления места
-const placeFormAdd = document.forms.place;
-// поля формы добавления места
-const placeNameField = placeFormAdd.elements.name;
-const placeLinkField = placeFormAdd.elements.link;
-
-function openPopupProfile() {
-  profilePopup.open();
-  inputName.value = profileName.textContent;
-  inputDescription.value = profileDescription.textContent;
-}
-
-function closeAndSaveProfile(e) {
-  e.preventDefault();
-  profileName.textContent = inputName.value;
-  profileDescription.textContent = inputDescription.value;
-  profilePopup.close();
-}
-
-function closeAndSavePlace(e) {
-  e.preventDefault();
-  let name = placeFormAdd.elements.name.value;
-  let link = placeFormAdd.elements.link.value;
-  let place = { name, link };
-  renderCard(place);
-  placePopup.close();
-}
-
-popupOpenProfileButton.addEventListener("click", openPopupProfile);
-popupOpenPlaceButton.addEventListener("click", function () {
-  placeNameField.value = "";
-  placeLinkField.value = "";
-  const popupPlace = document.querySelector("#popup-place");
-  const saveButton = popupPlace.querySelector(".popup__save-button");
-  saveButton.classList.add("popup__save-button_inactive");
-  saveButton.disabled = true;
-  placePopup.open();
-});
-
-profileFormEdit.addEventListener("submit", closeAndSaveProfile);
-placeFormAdd.addEventListener("submit", closeAndSavePlace);
-
+/** Функция отрисовки карточки */
 function renderCard(place) {
   const card = new Card("#place-card-template", place.name, place.link);
+  // добавляем карточку в начало сетки
   places.prepend(card.generateCard());
 }
 
+// делаем изначальную загрузку мест
 initialPlaces.forEach(renderCard);
